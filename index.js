03a0cb8c4a287ce5b9da61f6ca0e979ae2c19871
Narek Pedanyan <anushavan1993@gmail.com>
2020-09-16 15:55:15 +0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+const PI = Math.PI;
+const userInputRadius = prompt("Please enter radius");
+const radiusNumber = parseInt(userInputRadius);
+const area = PI*(Math.pow(radiusNumber, 2));
+const perimetr = 2*PI*radiusNumber;
+console.log(area, 'area');
+console.log(perimetr, 'perimetr');
+
