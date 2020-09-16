a60d684d3853beca7146e16eee268746ae625b9b
Agassi <agassipedanyan@gmail.com>
2020-09-16 16:06:09 +0400

    Issue fixed.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const PI = Math.PI;
 const userInputRadius = prompt("Please enter radius");
-const radiusNumber = parseInt(userInputRadius);
+const radiusNumber = parseFloat(userInputRadius);
 const area = PI*(Math.pow(radiusNumber, 2));
 const perimetr = 2*PI*radiusNumber;
 console.log(area, 'area');
